39e23bdb6ba52a87d560ad965dc4aeb3d3ccb07a
Yunus Korkmaz <yunuskorkmaz95@gmail.com>
2021-11-30 19:23:30 +0300

    move function is complete

--- src/move.ts
+++ src/move.ts
@@ -1,6 +1,32 @@
-// Please update this type as same as with the data shape.
-type List = unknown;
+type File = {
+  id: string;
+  name: string;
+};
+
+type Folder = {
+  id: string;
+  name: string;
+  files: Array<File>;
+};
+
+export type List = Array<Folder>;
 
 export default function move(list: List, source: string, destination: string): List {
-  throw new Error('Not implemented');
+  const sourceFolder = list.find((folder) => folder.files.find((file) => file.id === source));
+  const destinationFolder = list.find((folder) => folder.id === destination);
+
+  if (!sourceFolder) {
+    throw new Error('You cannot move a folder');
+  }
+
+  if (!destinationFolder) {
+    throw new Error('You cannot specify a file as the destination');
+  }
+
+  const sourceFileIndex = sourceFolder.files.findIndex((file) => file.id === source);
+
+  destinationFolder.files.push(sourceFolder.files[sourceFileIndex]);
+  sourceFolder.files.splice(sourceFileIndex, 1);
+
+  return list;
 }
